7b385a99bd79c253fb3fca53675541901df1e93d
Hunter <muratoreh@gmail.com>
2023-11-06 11:14:41 -0500

    create putDb and getDb functions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,33 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('jate_db', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
+      if (db.objectStoreNames.contains('jate_store')) {
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jate_store', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB('jate_db', 1);
+  const tx = db.transaction('jate_store', 'readwrite');
+  const store = tx.objectStore('jate_store');
+  await store.add(content);
+  await tx.done;
+  console.log('Added content to the database:', content);
+}
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('jate_db', 1);
+  const tx = db.transaction('jate_store', 'readonly');
+  const store = tx.objectStore('jate_store');
+  const content = await store.getAll();
+  await tx.done;
+  console.log('Retrieved content from the database:', content);
+}
 
 initdb();
